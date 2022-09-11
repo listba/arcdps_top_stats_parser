f811e50141514328a88b006f3df168817627e29a
drevarr <drevarr@gmail.com>
2022-09-11 16:14:01 -0400

    added control effect output to xls

--- TW5_parse_top_stats_detailed.py
+++ TW5_parse_top_stats_detailed.py
@@ -415,6 +415,8 @@ if __name__ == '__main__':
 					i=i+1
 
 			myprint(output, "</$reveal>\n")
+
+		write_control_effects_xls(sorted_squadControl, key, players, args.xls_output_filename)
 	#end Control Effects Outgoing insert
 
 	for stat in config.stats_to_compute:
